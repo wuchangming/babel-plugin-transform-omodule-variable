c8f47ad2e64266434b94e23432fdf3e35801f3b1
wuchangming <wuchangmingnice@163.com>
2018-10-08 23:56:45 +0800

    support babel-plugin-transform-omodule-namespace

--- omodule-test-cases/root/route/route.js
+++ omodule-test-cases/root/route/route.js
@@ -2,3 +2,4 @@ const omodule_name = __omodule_name;
 const omodule_namespace = __omodule_namespace;
 const omodule_childnames = __omodule_childnames;
 const omodule_filename = __omodule_filename;
+const __ofilepath = __ofilepath
--- src/index.js
+++ src/index.js
@@ -1,5 +1,74 @@
 import path from 'path';
 import fs from 'fs';
+import appRoot from 'app-root-path'
+
+//// support omodule-namespace --- begin
+const projectRootPath = appRoot.path
+const OMODULE_CHILDREN_FOLDERNAME = 'omodules'
+const SEPARATOR = '/'
+
+function isAbsolutePath(p) {
+    return path.isAbsolute(p)
+}
+
+function createAbsolutePath(p) {
+    if (isAbsolutePath(p)) {
+        return path.normalize(p)
+    } else {
+        return path.normalize(path.join(projectRootPath, p))
+    }
+}
+
+function getFolderName(p) {
+    return path.join(p, '..')
+}
+
+function getONamesapce(matchRootPath, absoluteFilename, namespacePrefix) {
+    let onamespace
+    if (matchRootPath === getFolderName(absoluteFilename)) {
+        onamespace = namespacePrefix + '/'
+    } else {
+        const subPath = absoluteFilename.replace(matchRootPath, '')
+        const onamespaceResult = subPath
+            .split(`${path.sep}${OMODULE_CHILDREN_FOLDERNAME}${path.sep}`)
+            .reduce(
+                (acc, curr) => {
+                    if (acc.done) {
+                        return { ...acc }
+                    }
+
+                    if (curr) {
+                        if (curr.indexOf(path.sep) > -1) {
+                            const onamespace = acc.onamespace + curr.split(path.sep)[0] + '/'
+                            return {
+                                onamespace,
+                                done: true
+                            }
+                        } else {
+                            return { ...acc, ...{ onamespace: acc.onamespace + curr + '/' } }
+                        }
+                    } else {
+                        return { ...acc, ...{ onamespace: '/' } }
+                    }
+                },
+                {
+                    onamespace: '',
+                    done: false
+                }
+            )
+        onamespace = namespacePrefix + onamespaceResult.onamespace
+    }
+    return onamespace.length > 1 ? onamespace.replace(/\/$/, '') : onamespace
+}
+function getOFilePath(matchRootPath, absoluteFilename) {
+    return absoluteFilename.replace(matchRootPath + path.sep, '')
+}
+
+let done__onamespace = false
+let done__ofilepath = false
+
+//// support omodule-namespace --- end
+
 
 const isFile = pathString => {
     return fs.lstatSync(pathString).isFile();
@@ -7,10 +76,6 @@ const isFile = pathString => {
 
 const isDirectory = pathString => {
     return fs.lstatSync(pathString).isDirectory();
-};
-
-const getFolderName = pathString => {
-    return path.join(pathString, '..');
 };
 
 const isRoot = (dirname, sourceRoot) => {
@@ -120,6 +185,12 @@ const getOmoduleChildNames = (filename, sourceRoot, oFilename, oChildFolderName)
 export default function({ types: t }) {
     return {
         visitor: {
+            Program: {
+                enter() {
+                    done__onamespace = false
+                    done__ofilepath = false
+                }
+            },
             Identifier(babelPath, state) {
                 let { filename, childFolderName, separator } = state.opts;
                 const oFilename = filename || 'omodule.js';
@@ -165,6 +236,45 @@ export default function({ types: t }) {
                             })
                         )
                     );
+                }
+
+                const { rootPath, namespacePrefix = '' } = state.opts
+                let absoluteRootPath
+                if (rootPath) {
+                    if (typeof rootPath === 'string') {
+                        absoluteRootPath = createAbsolutePath(rootPath)
+                    } else {
+                        throw new Error('rootPath must be a string. Received ' + typeof rootPath)
+                    }
+                } else {
+                    absoluteRootPath = projectRootPath
+                }
+
+                const absoluteFilename = createAbsolutePath(state.file.opts.filename)
+
+                const matchRootPath = path.normalize(absoluteRootPath).replace(/[\\\/]+$/, '')
+
+                if (absoluteFilename.indexOf(absoluteRootPath) > -1) {
+                    if (babelPath.node.name === '__onamespace' && !done__onamespace) {
+                        const s = getONamesapce(matchRootPath, absoluteFilename, namespacePrefix)
+                        const prog = babelPath.find(path => path.isProgram())
+                        const ident = t.identifier('__onamespace')
+                        const string = t.stringLiteral(s)
+                        const declarator = t.variableDeclarator(ident, string)
+                        const declaration = t.variableDeclaration('var', [declarator])
+                        prog.scope.block.body.unshift(declaration)
+                        done__onamespace = true
+                    }
+                    if (babelPath.node.name === '__ofilepath' && !done__ofilepath) {
+                        const s = getOFilePath(matchRootPath, absoluteFilename)
+                        const prog = babelPath.find(path => path.isProgram())
+                        const ident = t.identifier('__ofilepath')
+                        const string = t.stringLiteral(s)
+                        const declarator = t.variableDeclarator(ident, string)
+                        const declaration = t.variableDeclaration('var', [declarator])
+                        prog.scope.block.body.unshift(declaration)
+                        done__ofilepath = true
+                    }
                 }
             }
         }
